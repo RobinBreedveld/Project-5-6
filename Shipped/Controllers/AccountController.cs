d2721aa6c96e2875fadc9f21d6e1fb4153214048
Oguzhan K <o.kelleci@live.nl>
2017-12-16 19:38:29 +0100

    Implemented E-Mail system

--- Shipped/Controllers/AccountController.cs
+++ Shipped/Controllers/AccountController.cs
@@ -230,7 +230,7 @@ namespace login2.Controllers
                     var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
                     await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
 
-                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    //await _signInManager.SignInAsync(user, isPersistent: false);
                     _logger.LogInformation("User created a new account with password.");
                     return RedirectToLocal(returnUrl);
                 }
--- Shipped/Services/EmailSender.cs
+++ Shipped/Services/EmailSender.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
 
 namespace login2.Services
 {
@@ -11,6 +13,23 @@ namespace login2.Services
     {
         public Task SendEmailAsync(string email, string subject, string message)
         {
+
+            using (MailMessage mail = new MailMessage())
+            {
+                mail.From = new MailAddress("project3informatica@gmail.com");
+                mail.To.Add($"{email}");
+                mail.Subject = $"{subject}";
+                mail.Body = $"<h1>{message}</h1>";
+                mail.IsBodyHtml = true;
+                //mail.Attachments.Add(new Attachment("C:\\file.zip"));
+
+                using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    smtp.Credentials = new NetworkCredential("project3informatica@gmail.com", "wollahpatrick");
+                    smtp.EnableSsl = true;
+                    smtp.Send(mail);
+                }
+            }
             return Task.CompletedTask;
         }
     }
